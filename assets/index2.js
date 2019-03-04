3247d65d90125b69ba6f957b2ad6294114119cc9
Brice Colucci <brice.colucci@logmein.com>
2019-03-04 14:26:12 -0500

    wip

--- assets/index2.js
+++ assets/index2.js
@@ -1,14 +1,11 @@
 // pages/index.js
-import { Component } from 'react'
 import { Container } from 'reactstrap'
 import EmployeesDropdown from '../components/EmployeesDropdown'
 
-export default class extends Component {
-  render () {
-    return (
-      <Container>
-        <EmployeesDropdown />
-      </Container>
-    )
-  }
+export default () => {
+  return (
+    <Container>
+      <EmployeesDropdown />
+    </Container>
+  )
 }
